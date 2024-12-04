ad2d788c0f9353db21cadcb0dae49c354e43a6a5
xvanthony <112587667+xvanthony@users.noreply.github.com>
2024-12-03 23:29:37 -0800

    fixed

--- app/models/priority_sortable.rb
+++ app/models/priority_sortable.rb
@@ -1,24 +1,24 @@
 module PrioritySortable
     extend ActiveSupport::Concern
-  
+
     included do
       before_save :set_default_priority
     end
-  
+
     PRIORITY_SCORES = {
       "High" => 3,
       "Medium" => 2,
       "Low" => 1
     }.freeze
-  
-    #def set_default_priority
-      #self.priority ||= "Medium"
-    #end
-  
+
+    def set_default_priority
+      self.priority ||= "Medium"
+    end
+
     def urgency_score
       PRIORITY_SCORES[priority] || 0
     end
-  
+
     module ClassMethods
       def sort_by_priority(custom_logic = nil)
         all.sort_by do |todo|
@@ -26,5 +26,4 @@ module PrioritySortable
         end
       end
     end
-  end
-  
+end
